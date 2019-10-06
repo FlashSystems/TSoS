2f5cb74cc6a8289800b3ba790d6e04d8562b04fa
Daniel Goß <developer@flashsystems.de>
2019-10-06 13:25:04 +0200

    Make journal_logger compatible with other platforms
    On other platforms the journal_logger::has_journal() call will always
    return false and journal_logger::init_with_level() will always panic.

--- journal_logger/src/lib.rs
+++ journal_logger/src/lib.rs
@@ -4,11 +4,20 @@ use std::os::unix::io::{RawFd, AsRawFd};
 use std::mem::MaybeUninit;
 use std::io::{stdout, stderr};
 
+#[cfg(target_os = "linux")]
 mod logger;
+#[cfg(target_os = "linux")]
 pub use logger::init_with_level;
+
+#[cfg(not(target_os = "linux"))]
+pub fn init_with_level(level: Level) -> Result<(), SetLoggerError> {
+	panic!("Called journal_logger::init_with_level on an unsupported platform. Use has_journal to prevent hat.");
+}
+
 
 /// Checks that the device and inode number of the passed RawFd match the 
 /// values in device and inode. If an error occures false is returned.
+#[cfg(target_os = "linux")]
 fn check_descriptor(device: libc::dev_t, inode: libc::ino_t, fd: RawFd) -> bool{
 	unsafe { 
 		let mut stat = MaybeUninit::<libc::stat64>::zeroed();
@@ -25,6 +34,7 @@ fn check_descriptor(device: libc::dev_t, inode: libc::ino_t, fd: RawFd) -> bool{
 /// JOURNAL_STREAM environment variable. If that's the case we return true and the
 /// main program should upgrade to journald logging. If an error occures or stdout/stderr
 /// are not connected to the journal we return false.
+#[cfg(target_os = "linux")]
 pub fn has_journal() -> bool {
 	if let Ok(journal_stream) = env::var("JOURNAL_STREAM") {
 		if let [Some(device), Some(inode)] = journal_stream.split(":").map(|v| u64::from_str_radix(v.trim(), 10).ok()).collect::<Vec<Option<u64>>>()[0..2] {
@@ -37,4 +47,10 @@ pub fn has_journal() -> bool {
 	} else {
 		false
 	}
+}
+
+/// If we're not on linux, we always return false because systemd is only available on linux.
+#[cfg(not(target_os = "linux"))]
+pub fn has_journal() -> bool {
+	false
 }
